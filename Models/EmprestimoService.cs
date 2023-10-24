5a941dd45115b5f79af5d948cb79a09b641d418a
inove-tec <ronaldinoveti@gmail.com>
2023-10-24 09:21:26 -0300

    Primeiro Envio

---
+++ Models/EmprestimoService.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Models
+{
+    public class EmprestimoService 
+    {
+        public void Inserir(Emprestimo e)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                bc.Emprestimos.Add(e);
+                bc.SaveChanges();
+            }
+        }
+
+        public void Atualizar(Emprestimo e)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                Emprestimo emprestimo = bc.Emprestimos.Find(e.Id);
+                //emprestimo.NomeUsuario = e.NomeUsuario;
+                //emprestimo.Telefone = e.Telefone;
+                emprestimo.LivroId = e.LivroId;
+                emprestimo.DataEmprestimo = e.DataEmprestimo;
+                emprestimo.DataDevolucao = e.DataDevolucao;
+
+                bc.SaveChanges();
+            }
+        }
+
+        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro = null)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                IQueryable<Emprestimo> query;
+
+                if(filtro != null)
+                {
+                    //definindo a filtragem
+
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "Usuario":
+                            query = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro));
+                        break;
+
+                        case "Livro":
+                            query = bc.Emprestimos.Where(e => e.Livro.Titulo.Contains(filtro.Filtro));
+                        break;
+
+                        default:
+                            query = bc.Emprestimos;
+                        break;
+                    }
+                }
+                else
+                {
+                    //filtro não informado
+                    query = bc.Emprestimos;
+                }
+
+                return query.Include(e => e.Livro).OrderBy(e => e.DataDevolucao).ToList();
+                //return bc.Emprestimos.Include(e => e.Livro).ToList();
+            }
+        }
+
+        public Emprestimo ObterPorId(int id)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Emprestimos.Find(id);
+            }
+        }
+    }
+}
